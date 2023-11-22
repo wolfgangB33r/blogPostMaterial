9fef1fc5a16dfdb66bd4c90a0fdf4af9164f287b
Wolfgang Beer <wolfgang.beer@dynatrace.com>
2023-11-22 09:50:20 +0100

    added traceloop blog post examples

--- openllmetry/chaining.py
+++ openllmetry/chaining.py
@@ -4,17 +4,28 @@ import openai
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+from traceloop.sdk.decorators import workflow, task
 
 Traceloop.init(app_name="openai-obs", disable_batch=True)
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-llm = OpenAI(openai_api_key=openai.api_key)
+@task(name="add_prompt_context")
+def add_prompt_context():
+    llm = OpenAI(openai_api_key=openai.api_key)
+    prompt = ChatPromptTemplate.from_template("explain the business of company {company} in a max of {length} words")
+    model = ChatOpenAI()
+    chain = prompt | model
+    return chain
 
-chat_model = ChatOpenAI()
+@task(name="prep_prompt_chain")
+def prep_prompt_chain():
+    return add_prompt_context()
 
-prompt = ChatPromptTemplate.from_template("tell me a joke about {foo}")
-model = ChatOpenAI()
-chain = prompt | model
+@workflow(name="ask_question")
+def prompt_question():
+    chain = prep_prompt_chain()
+    return chain.invoke({"company": "dynatrace", "length" : 50})
 
-print(chain.invoke({"foo": "bears"}))
+if  __name__ == "__main__":
+    print(prompt_question())
