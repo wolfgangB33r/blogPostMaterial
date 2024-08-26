3e2dee1fe51b44d8ba9c90fcc2952dff7dd435bc
Wolfgang Beer <wolfgang@smartlab.at>
2024-08-26 20:00:53 +0200

    Update image-gen.py

--- openai-image-generation/image-gen.py
+++ openai-image-generation/image-gen.py
@@ -1,4 +1,4 @@
-import openai
+from openai import OpenAI
 import os
 
 # Asking the user for a string input
@@ -7,13 +7,12 @@ user_input = input("Please describe the image you want to generate: ")
 # Displaying the user's input
 print("You entered:", user_input)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key = os.environ.get("OPENAI_API_KEY"),)
 
-
-response = openai.Image.create(
+response = client.images.generate(
   model="dall-e-3",
   prompt=user_input,
-  size="1024x1024",
+  size="1024x1792",
   quality="standard",
   n=1,
 )
